b5ebaf7db0c4048279b80b40733bc95271a35186
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-23 16:15:39 -0700

    alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books (
+	book_id PRIMARY KEY,
+	title VARCHAR(130),
+	FOREIGN KEY (author_id) REFRENCES Authors(author_id),
+	price FLOAT,
+	publication_date DATE
+);
+
+CREATE TABLE Authors (
+	author_id PRIMARY KEY,
+	author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+	customer_id PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+CREATE TABLE Orders (
+	order_id PRIMARY _KEY,
+	FOREIGN KEY (customer_id) REFRENCES Customers(customer_id),
+	order_date DATE
+);
+
+CREATE TABLE Order_Details (
+	orderdetailid PRIMARY KEY,
+	FOREIGN KEY (order_id) REFRENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFRENCES Books(book_id),
+	quantity FLOAT
+);
