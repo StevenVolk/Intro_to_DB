8b3b5d73567bc0bfce3feab79b284ae1882f9ec4
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-24 05:31:09 -0700

    MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,7 @@
-import mysql.connect
+import mysql.connector
 
 CREATE DATABASE IF NOT EXISTS alx_book_store
+
+alx_book_store = mysql.connector.connect(database="alx_book_store")
 
 alx_book_store.close()
