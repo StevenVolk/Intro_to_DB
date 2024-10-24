2c0c4de595ceae05b4159f4db1de66fb0f9bf17e
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-24 04:48:31 -0700

    changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,9 +2,4 @@ import mysql.connector
 
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-alx_book_store = mysql.connector.connect(
-        host="localhost",
-        user="yourusername",
-        password="yourpassword",
-        database="alx_book_store"
-)
+alx_book_store.close()
--- task_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE table_name = 'books'
-  [AND table_schema = 'alx_book_store']
-  [AND column_name LIKE 'wild']
-SHOW COLUMNS
-  FROM tbl_name
-  [FROM db_name]
-  [LIKE 'wild']
