9824bb50db49942c961a696feb68d6fcf41e4380
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-24 04:55:23 -0700

    changes

--- task_4.sql
+++ task_4.sql
@@ -1,9 +1,9 @@
 SELECT COLUMN_NAME, COLUMN_TYPE, TABLE_SCHEMA, TABLE_NAME
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE table_name = 'books'
+WHERE table_name = 'Books'
 [AND table_schema = 'alx_book_store']
 [AND column_name LIKE 'wild']
 SHOW COLUMNS
-FROM 'books'
+FROM 'Books'
 [FROM 'alx_book_store']
 [LIKE 'wild']
