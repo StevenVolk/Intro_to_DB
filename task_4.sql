38948d0635ca8a88cc64e066aa3c1eb89640ab96
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-24 04:39:00 -0700

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,9 @@
-USE alx_book_store;
-DESC books;
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'books'
+  [AND table_schema = 'alx_book_store']
+  [AND column_name LIKE 'wild']
+SHOW COLUMNS
+  FROM tbl_name
+  [FROM db_name]
+  [LIKE 'wild']
